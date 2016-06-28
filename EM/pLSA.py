30f853124bd41b7ff55b00043e110d5e3ba40977
jz3707 <ans3707@126.com>
2016-06-29 06:16:53 +0800

    更新plsa 和utils 20160629

--- EM/pLSA.py
+++ EM/pLSA.py
@@ -3,3 +3,79 @@
 
 import re
 import numpy as np
+
+
+class Document(object):
+    def __init__(self, filepath):
+        """
+        设置文件路径，初始化
+        :return:
+        """
+        self.filepath = filepath
+
+        self.lines = []
+        self.words = []
+
+        self.file = open(self.filepath)
+        try:
+            self.lines = [line for line in self.file]
+        finally:
+            self.file.close()
+
+    def split(self):
+        """
+        读取分词结果
+        :return:
+        """
+        for line in self.lines:
+            words = line.split(" ")
+            for word in words:
+                self.words.append(word)
+
+
+class Corpus(object):
+    """
+    a collection of document
+    """
+
+    def __init__(self):
+        self.documents = []
+        self.vocabulary = []
+
+    def add_document(self, document):
+        self.documents.append(document)
+
+    def build_vocabulary(self):
+        """
+        构建一个词表，词表中的词就是语料中的去重词
+        :return:
+        """
+        discreate_set = set()
+        for document in self.documents:
+            for word in document.words:
+                discreate_set.add(word)
+        self.vocabulary = list(discreate_set)
+
+    def plsa(self, nb_topics, max_iter):
+        """
+        EM
+        :param nb_topics:  numbertof topic
+        :param max_iter: max number of iterations
+        :return:
+        """
+        print("EM iteration begins....")
+        self.build_vocabulary()
+        nb_of_documents = len(self.documents)
+        vocab_size = len(self.vocabulary)
+
+        # 构建词-文档矩阵
+        term_doc_matrix = np.zeros([nb_of_documents, vocab_size], dtype=np.int)
+        for d_index, doc in enumerate(self.documents):
+            term_count = np.zeros(vocab_size, dtype=np.int)
+            for word in doc.words:
+                if word in self.vocabulary:
+                    w_index = self.vocabulary.index(word)
+                    term_count[w_index] += 1
+            term_doc_matrix[d_index] = term_count
+
+
--- EM/utils.py
+++ EM/utils.py
@@ -3,3 +3,43 @@
 
 from random import gammavariate
 from random import random
+
+
+"""
+Alpha use a Gamma Distribution.
+Samples from a Dirichlet Distribution with alpha.
+"""
+def Dirichlet(alpha):
+    sample = [gammavariate(a, 1) for a in alpha]
+    sample = [v / sum(sample) for v in sample]
+    return sample
+
+"""
+Normalize a vector to be a probablistic representation
+"""
+def normalize(vec):
+    s = sum(vec)
+    assert (abs(s) != 0.0)
+
+    for i in range(len(vec)):
+        assert (vec[i] >- 0)
+        vec[i] = vec[i] * 1.0 / s
+
+
+"""
+Choose a element in @vec according to a specified distribution @pr
+"""
+def choose(vec, pr):
+    assert (len(vec) == len(pr))
+    # normalize the distribution
+    normalize(vec)
+    r = random()
+    index = 0
+    while r > 0:
+        r = r - pr[index]
+        index += 1
+    return vec[index - 1]
+
+if __name__ == '__main__':
+    # test
+    print(Dirichlet([1, 1, 1]))
